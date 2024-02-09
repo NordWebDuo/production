e71aaf06b60eacda9847558e4b5547749c7fa86d
NordWebDuo <contact@nordwebduo.com>
2024-02-09 13:55:20 +0000

    bug fix

--- components/Layout.js
+++ components/Layout.js
@@ -19,7 +19,6 @@ const Layout = ({ children, pageTitle, pageDescription, ogPageTitle,metaImage,me
         <meta property="og:type" content="website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" sizes="32x32" href="https://app.nordwebduo.com/uploads/favicon_32x32_88df5d0db5.png" />
-        <meta http-equiv="Content-Security-Policy" content="script-src 'none'" />
     </Head>
     <Header />
     <main>
